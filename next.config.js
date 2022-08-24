26374b0543ccb282bef8892789f09a26e0ca54f9
Sashe Vuchkov <sashevuchkov@gmail.com>
2022-08-25 02:42:00 +0300

    Deleted some js files we don't need

--- next.config.js
+++ next.config.js
@@ -4,12 +4,6 @@ let {merge} = require('webpack-merge');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack: (config) => {
-    // this will override the experiments
-    config.experiments = { topLevelAwait: true };
-    // config.experiments.topLevelAwait = true
-    return config;
-  },
 }
 
 module.exports = nextConfig
